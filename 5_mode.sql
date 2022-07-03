c00d76d9a1ff77126ea4029a36ef56ced0a4d19c
vbourassa <bourassa.v@gmail.com>
2022-07-03 21:34:09 +0200

    Only a few try it yourself exercices

---
+++ 5_mode.sql
@@ -0,0 +1,2 @@
+select mode() within group (order by p0010001)
+from us_counties_2010;
---
+++ 5_unnest.sql
@@ -0,0 +1,5 @@
+select unnest(
+    percentile_cont(array[.25, .5, .75])
+    within group (order by p0010001)
+) as quartiles
+from us_counties_2010;
