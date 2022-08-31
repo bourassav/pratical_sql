dee6d734d41eaff166f57260e23a88340c19238e
vbourassa <bourassa.v@gmail.com>
2022-08-31 09:12:46 +0200

    Continuing chapter 10

---
+++ 10_create_store_sales.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS store_sales;
+
+CREATE TABLE store_sales(
+    store varchar(30),
+    category varchar(30) NOT NULL,
+    unit_sales bigint NOT NULL,
+    CONSTRAINT store_category_key PRIMARY KEY (store, category)
+);
+
+INSERT INTO
+    store_sales (store, category, unit_sales)
+VALUES
+    ('Broders', 'Cereal', 1104),
+    ('Wallace', 'Ice Cream', 1863),
+    ('Broders', 'Ice Cream', 2517),
+    ('Cramers', 'Ice Cream', 2112),
+    ('Broders', 'Beer', 641),
+    ('Cramers', 'Cereal', 1003),
+    ('Cramers', 'Beer', 640),
+    ('Wallace', 'Cereal', 980),
+    ('Wallace', 'Beer', 988);
---
+++ 10_create_widgets.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS widget_companies;
+
+CREATE TABLE widget_companies(
+    id bigserial,
+    company varchar(30) NOT NULL,
+    widget_output integer NOT NULL
+);
+
+INSERT INTO
+    widget_companies (company, widget_output)
+VALUES
+    ('Morse Widgets', 125000),
+    ('Springfield Widgets Masters', 143000),
+    ('Best Widgets', 196000),
+    ('Acme Inc.', 133000),
+    ('District Widget Inc.', 201000),
+    ('Clarke Amalgamated', 620000),
+    ('Stavesacre Industries', 244000),
+    ('Bowers Widget Emporium', 201000);
